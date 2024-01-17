ff1fe63676981b50c4cc03cdfa6d47b65e84b22b
Mclovinx99 <aditya.rathi.hestabit@gmail.com>
2024-01-17 18:54:43 +0530

    added jwt, logins, some crud endpoints

--- controller/login_signup.js
+++ controller/login_signup.js
@@ -5,9 +5,8 @@ const jwt = require('jsonwebtoken');
 const multer  = require('multer');
 const upload = multer({ dest: 'uploads/' });
 
-module.exports.user = async(req,res) =>{
+module.exports.create_admin = async(req,res) =>{
     const {password, name, email} = req.body;
-
     try{
         const createdUser = await prisma.Admin.create({
             data:{
@@ -23,6 +22,51 @@ module.exports.user = async(req,res) =>{
         console.log(err);
     }finally{
         await prisma.$disconnect();
+    }
+}
+
+module.exports.login = async(req,res)=>{
+    const {email, password} = req.body;
+    try{
+        const user = await prisma.Admin.findUnique({
+            where: {
+                email: email
+            }
+        });
+        console.log(user);
+        if(bcrypt.compareSync(user && password,user.Password)){
+            const token = jwt.sign({name:user.name},'se3ret',{expiresIn:3600})
+            res
+            .status(200)
+            .json({message:'user found',token:token});
+        }else{
+            res
+            .status(401)
+            .json({message:'Incorrect password'});
+        }
+
+    }catch(err){
+        console.log("err" +err);
+    }
+}
+
+module.exports.authorize = async(req,res,next)=>{
+    var headerExist = req.header.authorization;
+    if(headerExist){
+        let token = headerExist.split('.');
+        jwt.verify(token,'se3ret',(decoded,err)=>{
+            if(decoded)
+            {
+                res
+                .status(200)
+                .next();
+            }
+            else{
+                res
+                .status(401)
+                .json({message:'not allowed to access.'})
+            }
+        })
     }
 }
 
--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,14 @@ const add_user = require('../controller/login_signup');
 
 Router
 .route('/create')
-.post(add_user.user);
+.post(add_user.create_admin);
 
 Router
 .route('/image')
 .post(add_user.upload);
+
+Router
+.route('/login')
+.post(add_user.login);
 
 module.exports = Router;
